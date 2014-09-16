6b78bdda663bfa4c0a5ac52680ff0ad2451cf9fb
Johan Leth Gregersen <Johangregersen@live.dk>
2014-09-16 11:09:04 +0200

    set up a postgreSQL database, and made it possible to write to it - database on Laustsens computer.

--- Seven/app/src/main/java/sw7/cornfield/MainActivity.java
+++ Seven/app/src/main/java/sw7/cornfield/MainActivity.java
@@ -6,9 +6,16 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.sql.Statement;
+import java.sql.*;
+import java.util.*;
 
 public class MainActivity extends Activity {
 
@@ -21,10 +28,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+
         // Set context for the main activity to be used internally in this class
         mainContext = this.getApplicationContext();
         GpsApi gps = new GpsApi();
         locMan = gps.askForGps(mainContext);
+
+        tempButtons();
 
         onLocationChange = new LocationListener() {
             public void onLocationChanged(Location location) {
@@ -47,8 +59,43 @@ public class MainActivity extends Activity {
         TextView lat = (TextView) findViewById(R.id.lat);
         TextView lon = (TextView) findViewById(R.id.lng);
 
-        lat.setText(String.format("%f",loc.getLatitude()));
-        lon.setText(String.format("%f",loc.getLongitude()));
+        lat.setText(String.format("%f", loc.getLatitude()));
+        lon.setText(String.format("%f", loc.getLongitude()));
+    }
+
+    public void tempButtons() {
+        Button knap = (Button)findViewById(R.id.knap);
+        knap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Client user = new Client();
+            }
+        });
+
+        Button dbKnap = (Button)findViewById(R.id.dbknap);
+        dbKnap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    insertDB();
+                    System.out.println("Succes");
+                }catch(Exception e){
+                    System.out.println("it broke");
+                    System.out.println(e.toString());
+                }
+            }
+        });
+    }
+
+    public void insertDB() throws SQLException {
+        dbCon con = new dbCon();
+        Statement st = con.DBC.createStatement();
+        String name = "johan1";
+        String dist = "johansfar1";
+        String ins = "INSERT INTO \"public\".\"usertable\"(\"distributer\",\"phonetype\") VALUES('" + dist + "' ,'" + name + "')";
+        //String ins = "INSERT INTO public.usertable(distributer,phonetype) VALUES (" + dist + "," + "name)";
+        st.execute(ins);
+        con.Close();
     }
 
     @Override
