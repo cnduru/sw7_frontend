dcd9d15121741f80285bb18a5a4c592fc11a9371
Morten Jakobsen <treeline@live.dk>
2014-11-13 19:57:29 +0100

    Don't ask, but there's an activity for signing up now

--- Seven/app/src/main/java/sw7/cornfield/LoginActivity.java
+++ Seven/app/src/main/java/sw7/cornfield/LoginActivity.java
@@ -3,35 +3,108 @@ package sw7.cornfield;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
-    EditText username;
-    EditText password;
-    Button loginButton;
-    Button createUser;
+    EditText UsernameText;
+    EditText PasswordText;
+    Button LoginButton;
+    Button SignUpButton;
+    Boolean UsernameValid = false;
+    Boolean PasswordValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = (EditText) findViewById(R.id.Username);
-        password = (EditText) findViewById(R.id.Password);
-        loginButton = (Button) findViewById(R.id.Login);
-        createUser = (Button) findViewById(R.id.CreateUser);
+        UsernameText = (EditText) findViewById(R.id.Username);
+        PasswordText = (EditText) findViewById(R.id.Password);
+        LoginButton = (Button) findViewById(R.id.Login);
+        SignUpButton = (Button) findViewById(R.id.SignUp);
 
-        loginButton.setOnClickListener(loginListener);
+        UsernameText.addTextChangedListener(UsernameValidator);
+        PasswordText.addTextChangedListener(PasswordValidator);
+        LoginButton.setOnClickListener(LoginListener);
+        SignUpButton.setOnClickListener(SignUpListener);
     }
 
-    View.OnClickListener loginListener = new View.OnClickListener() {
+    private void updateLoginButton() {
+        if (UsernameValid && PasswordValid) {
+            LoginButton.setEnabled(true);
+        } else {
+            LoginButton.setEnabled(false);
+        }
+    }
+
+    TextWatcher UsernameValidator = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String username = editable.toString();
+
+            if (username.length() > 0) {
+                UsernameValid = true;
+            } else {
+                UsernameValid = false;
+            }
+            updateLoginButton();
+        }
+    };
+
+    TextWatcher PasswordValidator = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String password = editable.toString();
+
+            if (password.length() > 0) {
+                PasswordValid = true;
+            } else {
+                PasswordValid = false;
+            }
+            updateLoginButton();
+        }
+    };
+
+    View.OnClickListener LoginListener = new View.OnClickListener() {
         public void onClick(View v) {
-            Intent intent = new Intent(LoginActivity.this, OverviewActivity.class);
-            intent.putExtra("Username", username.getText().toString());
+            //If user and password is found in DB, login. Otherwise display error.
+            if(true) {
+                Intent intent = new Intent(LoginActivity.this, OverviewActivity.class);
+                intent.putExtra("Username", UsernameText.getText().toString());
+                startActivity(intent);
+            } else {
+                Toast loginError = Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT);
+                loginError.show();
+                PasswordText.setText("");
+            }
+        }
+    };
+
+    View.OnClickListener SignUpListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(intent);
         }
     };
---
+++ Seven/app/src/main/java/sw7/cornfield/SignUpActivity.java
@@ -0,0 +1,149 @@
+package sw7.cornfield;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SignUpActivity extends Activity {
+
+    EditText UsernameText;
+    TextView UsernameStatusText;
+    ImageView UsernameStatusImage;
+    EditText PasswordText;
+    ImageView PasswordStatusImage;
+    EditText ConfirmPasswordText;
+    ImageView ConfirmPasswordStatusImage;
+    Button CreateUserButton;
+
+    Boolean UsernameValid = false;
+    Boolean PasswordValid = false;
+    Boolean PasswordsMatch = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        UsernameText = (EditText) findViewById(R.id.Username);
+        UsernameStatusText = (TextView) findViewById(R.id.UsernameStatusText);
+        UsernameStatusImage = (ImageView) findViewById(R.id.UsernameStatusImage);
+        PasswordText = (EditText) findViewById(R.id.Password);
+        PasswordStatusImage = (ImageView) findViewById(R.id.PasswordStatusImage);
+        ConfirmPasswordText = (EditText) findViewById(R.id.ConfirmPassword);
+        ConfirmPasswordStatusImage = (ImageView) findViewById(R.id.ConfirmPasswordStatusImage);
+        CreateUserButton = (Button) findViewById(R.id.CreateUser);
+
+        UsernameText.addTextChangedListener(UsernameValidator);
+        PasswordText.addTextChangedListener(PasswordValidator);
+        ConfirmPasswordText.addTextChangedListener(PasswordMatchValidator);
+        CreateUserButton.setOnClickListener(SignUpListener);
+    }
+
+    private void updateSignUpButton() {
+        if (UsernameValid && PasswordValid && PasswordsMatch) {
+            CreateUserButton.setEnabled(true);
+        } else {
+            CreateUserButton.setEnabled(false);
+        }
+    }
+
+    View.OnClickListener SignUpListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(SignUpActivity.this, OverviewActivity.class);
+            intent.putExtra("Username", UsernameText.getText().toString());
+            startActivity(intent);
+        }
+    };
+
+    TextWatcher UsernameValidator = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String username = editable.toString();
+            if (username.length() > 0) {
+                //Check if username already exists in DB
+                Boolean alreadyExists = false;
+                if (alreadyExists) {
+                    UsernameValid = false;
+                    UsernameStatusImage.setImageResource(R.drawable.cross);
+                    UsernameStatusText.setText("The username " + username + " is already taken");
+
+                } else {
+                    UsernameValid = true;
+                    UsernameStatusImage.setImageResource(R.drawable.checkmark);
+                    UsernameStatusText.setText("The username " + username + " is available");
+                }
+            } else {
+                UsernameValid = false;
+            }
+            updateSignUpButton();
+        }
+    };
+
+    TextWatcher PasswordValidator = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String password = editable.toString();
+            if (password.length() >= 6 && ConfirmPasswordText.getText().toString().equals(password)) {
+                PasswordValid = true;
+                PasswordsMatch = true;
+                PasswordStatusImage.setImageResource(R.drawable.checkmark);
+                ConfirmPasswordStatusImage.setImageResource(R.drawable.checkmark);
+            } else if (password.length() >= 6) {
+                PasswordValid = true;
+                PasswordsMatch = false;
+                PasswordStatusImage.setImageResource(R.drawable.checkmark);
+                ConfirmPasswordStatusImage.setImageResource(R.drawable.cross);
+            } else{
+                PasswordValid = false;
+                PasswordStatusImage.setImageResource(R.drawable.cross);
+            }
+            updateSignUpButton();
+        }
+    };
+
+    TextWatcher PasswordMatchValidator = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String confirmPassword = editable.toString();
+            if (confirmPassword.equals(PasswordText.getText().toString())) {
+                PasswordsMatch = true;
+                ConfirmPasswordStatusImage.setImageResource(R.drawable.checkmark);
+            } else {
+                PasswordsMatch = false;
+                ConfirmPasswordStatusImage.setImageResource(R.drawable.cross);
+            }
+            updateSignUpButton();
+        }
+    };
+}
